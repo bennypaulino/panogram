e5c3b8f6223ac8c3455933ce7c4b6ffe247785f9
bennypaulino <santiagopro71@gmail.com>
2016-09-20 14:20:30 -0400

    Add tests and validations for User model, & adjust config/environments/development.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Joe Schmoe", email: "joe@schmoe.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "a name should be present" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "an email address should be present" do
+    @user.email = "   "
+    assert_not @user.valid?
+  end
 end
