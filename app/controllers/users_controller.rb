b66e8e77fa3676688a8f54c07f26705db25ba623
bennypaulino <santiagopro71@gmail.com>
2016-09-30 14:39:05 -0400

    Generate Sessions controller & adjust routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,6 @@ class UsersController < ApplicationController
   def signup
   end
 
-  def login
-  end
-
-  def logout
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
