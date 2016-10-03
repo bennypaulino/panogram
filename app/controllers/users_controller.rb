a1a17d99a34bc0e716726f5b345e03202b0e1e53
bennypaulino <santiagopro71@gmail.com>
2016-10-03 18:17:51 -0400

    Allow users to successfully edit their profile
    * Add test/integration/users_edit_test.rb
    * Adjust the update action within users_controller.rb
    * Adjust User model so a password isn't necessary to update profile.
    * Adjust edit.html.erb to squash bug.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # def signup
-  # end
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -29,7 +26,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-       # handle a successful update
+       flash[:success] = "Your profile has been updated"
+       redirect_to @user
     else
       render 'edit'
     end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -16,4 +16,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
     assert_select "div.alert", "The form contains 4 errors"
   end
+
+  test "successful edit to user profile" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Krusty the Clown"
+    email = "krusty@theclown.com"
+    patch user_path(@user), params: { user: { name: name, email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
