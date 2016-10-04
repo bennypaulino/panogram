e324ee4d7bb644771a85989c1f13b7c1a4934a51
bennypaulino <santiagopro71@gmail.com>
2016-10-03 22:34:34 -0400

    Deny access to protected pages
    * Implement authorization that requires users to be logged in and
    prevents them form updating any information other than their own.
    * Add tests for test/controllers/users_controller_test.rb
    * Adjust test/integration/users_edit_test.rb
    * Add a before_action to controllers/users_controller.rb

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
   def setup
+    @user = users(:krusty)
     @base_title = "Because the world isn't always square"
   end
 
@@ -16,14 +18,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Sign Up | #{@base_title}"
   end
 
-  # test "should get login" do
-  #   get login_path
-  #   assert_response :success
-  # end
+  test "should redirect edit page when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 
-  # test "should get logout" do
-  #   get logout_path
-  #   assert_response :success
-  # end
-
+  test "should redirect any attempt to update a profile when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 end
