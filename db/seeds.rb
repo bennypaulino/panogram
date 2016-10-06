0433b1883fa10270290f3968ac97146474163193
bennypaulino <santiagopro71@gmail.com>
2016-10-06 11:22:22 -0400

    Add sample users and implement pagination
    * Adjust gemfile and add faker gem for the development environment.
    * Adjust db/seeds.rb and create fake users for the site.
    * Reset the database with the following command: rails db:migrate:reset
    * Seed the database with the following command: rails db:seed
    * Add will_paginate and bootstrap-will_paginate gems to the gemfile.
    * Adjust the index action within controllers/users_controller.rb
    * Adjust views/users/index.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Example User",
+             email: "example@panogram.com",
+             username: "blah",
+             password: "password",
+             password_confirmation: "password")
+
+50.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@panogram.com"
+  username = "reluctantracoon#{n+1}"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               username: username,
+               password: password,
+               password_confirmation: password)
+end
