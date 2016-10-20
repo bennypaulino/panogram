97ffc06ebdd716f11d99d004bb427193a425e547
bennypaulino <santiagopro71@gmail.com>
2016-10-20 15:36:49 -0400

    Allow users to destroy their own microposts
    * Adjust views/microposts/_micropost.html.erb and add delete link.
    * Adjust controllers/microposts_controller.rb - add a before_action,
    define the destroy action, and add a private method for correct_user.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,11 +1,12 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
-      redirect_to root_url
+      redirect_to request.referrer # root_url
     else
       @feed_items = []
       render 'static_pages/home'
@@ -13,11 +14,22 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_back(fallback_location: root_url)
+    # same as: redirect_to request.referrer || root_url
+    # request.referrer is the previous URL
   end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  # Checks that the current user actually has a micropost with the given id.
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
