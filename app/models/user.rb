c4c4a079f8a311979bc4ae0b71e742a89c44576c
bennypaulino <santiagopro71@gmail.com>
2016-10-20 15:05:08 -0400

    Display user's own feed of microposts on home-pg
    * Add a feed method to models/user.rb
    * Adjust controllers/static_pages_controller.rb
    * Create the partial views/shared/_feed.html.erb
    * Adjust controllers/microposts_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,6 +83,11 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # Select all microposts belonging to the current user
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
   # Converts email to all lower-case
