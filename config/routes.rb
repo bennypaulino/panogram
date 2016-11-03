4879b655be7863298d90bfa8f156426ec0af1b21
bennypaulino <santiagopro71@gmail.com>
2016-11-03 10:23:49 -0400

    Provide a web interface for following users
    * Adjust seeds.db to provide sample data.
    * Reset and reseed the database.
    * Adjust config/routes.rb
    * Create a partial for relationship stats views/shared/_stats.html.erb
    * Adjust views/static_pages/home.html.erb
    * Adjust assets/stylesheets/master.css
    * Create a follow/unfollow partial in views/users/_follow_form.html.erb
    * Adjust config/routes.rb & add create/destroy for relationships.
    * Create views/users/_follow.html.erb & views/users/_unfollow.html.erb
    * Adjust views/users/show.html.erb
    * Adjust test/integration/users_profile_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -16,10 +16,15 @@ Rails.application.routes.draw do
   get 'password_resets/new'
   get 'password_resets/edit'
 
-  resources :users #, except: [:new, :create]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
 
 
