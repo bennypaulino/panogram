4a1a9d1cb95e51b7aaaf2702d3ea460c028d5685
bennypaulino <santiagopro71@gmail.com>
2016-11-10 11:44:04 -0500

    Add styling, and adjust static_pages_controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,8 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page], per_page: 10)
+    else
+      redirect_to root_url
     end
   end
 
