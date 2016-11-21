0eae0981a186f9a6d0a17d1e82d1cc1e24fe4dfa
bennypaulino <santiagopro71@gmail.com>
2016-11-21 12:23:12 -0500

    Address issues with disliking a micropost

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -15,14 +15,9 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    #binding.pry
-    @liked_post = Like.find(params[:id])
-    if @liked_post.present?
-      @liked_post.destroy
-      #@user.dislike(@micropost)
-    end
+    @micropost = Micropost.find(params[:micropost_id])
+    current_user.dislike(@micropost)
 
-    #@user.dislike(@micropost)
     respond_to do |format|
       format.html { redirect_to current_user }
       format.js
